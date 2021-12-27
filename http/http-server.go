7fb5a6bf9e5110a446bb76336be202e9d4c37e89
abelb01 <44084096+abelb01@users.noreply.github.com>
2021-12-27 16:07:10 +0000

    updated http-server

--- http/http-server.go
+++ http/http-server.go
@@ -2,69 +2,49 @@ package main
 
 import (
         "fmt"
-        "html/template"
         "log"
         "net/http"
         "os"
-        "time"
-        "embed"
 )
 
-var resources embed.FS
-
-func logging(next http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                start := time.Now()
-                req := fmt.Sprintf("%s %s", r.Method, r.URL)
-                log.Println(req)
-                next.ServeHTTP(w, r)
-                log.Println(req, "completed in", time.Now().Sub(start))
-        })
-}
-
-// templates references the specified templates and caches the parsed results
-// to help speed up response times.
-var templates = template.Must(template.ParseFiles("/http/templates/base.html"))
-
-// index is the handler responsible for rending the index page for the site.
-func index() http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                b := struct {
-                        Title        template.HTML
-                        Name string
-                }{
-                        Title:        template.HTML("Business &verbar; Landing"),
-                        Name: "Trufflehog,",
-                }
-                err := templates.ExecuteTemplate(w, "base", &b)
-                if err != nil {
-                        http.Error(w, fmt.Sprintf("index: couldn't parse template: %v", err), http.StatusInternalServerError)
-                        return
-                }
-                w.WriteHeader(http.StatusOK)
-        })
-}
-
 func main() {
-        mux := http.NewServeMux()
-        mux.Handle("/", logging(index()))
+        logPath := "development.log"
+        httpPort := 9001
 
-        port, ok := os.LookupEnv("PORT")
-        if !ok {
-                port = "9001"
-        }
+        openLogFile(logPath)
 
-        addr := fmt.Sprintf(":%s", port)
-        server := http.Server{
-                Addr:         addr,
-                Handler:      mux,
-                ReadTimeout:  15 * time.Second,
-                WriteTimeout: 15 * time.Second,
-                IdleTimeout:  15 * time.Second,
-        }
-        log.Println("main: running simple server on port", port)
-        if err := server.ListenAndServe(); err != nil {
-                log.Fatalf("main: couldn't start simple server: %v\n", err)
+        log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+        http.HandleFunc("/", rootHandler)
+
+        fmt.Printf("listening on %v\n", httpPort)
+        fmt.Printf("Logging to %v\n", logPath)
+
+        err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort), logRequest(http.DefaultServeMux))
+        if err != nil {
+                log.Fatal(err)
         }
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "<h1>Hello World</h1><div>Welcome to whereever you are</div>")
+}
+
+func logRequest(handler http.Handler) http.Handler {
+        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+                handler.ServeHTTP(w, r)
+        })
+}
+
+func openLogFile(logfile string) {
+        if logfile != "" {
+                lf, err := os.OpenFile(logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+
+                if err != nil {
+                        log.Fatal("OpenLogfile: os.OpenFile:", err)
+                }
+
+                log.SetOutput(lf)
+        }
+}
